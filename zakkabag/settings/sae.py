c8bd09cf1d8a380f9e07f47bb34b1bb4c7fc0438
navicester <navicester@qq.com>
2017-08-21 14:20:29 +0800

    update to single DB

--- zakkabag/settings/sae.py
+++ zakkabag/settings/sae.py
@@ -12,6 +12,13 @@ MYSQL_USER = 'lno2o5x0ox' #ACCESSKEY
 MYSQL_PASS = 'jx3531ximhkk5z5h3l4myx41w330x3lyz0zz1x3m' #SECRETKEY
 MYSQL_DB   = 'app_zakkabag'
 
+
+MYSQL_HOST = 'mclkvoqfrfqv.mysql.sae.sina.com.cn'
+MYSQL_PORT = '10099'
+MYSQL_USER = 'bhe001'
+MYSQL_PASS = 'majia101214'
+MYSQL_DB   = 'szakkabag'
+
 if not 'SERVER_SOFTWARE' in os.environ:
 	import sae
 	from sae._restful_mysql import monkey
