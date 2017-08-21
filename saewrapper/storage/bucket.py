38768e2f4f63bdfa20a9def6f96236967eb7b00f
navicester <navicester@qq.com>
2017-08-21 15:11:50 +0800

    update

--- saewrapper/storage/bucket.py
+++ saewrapper/storage/bucket.py
@@ -1,12 +1,13 @@
 from sae import storage
 
 class SAEBucket(object):
+    conn = None
     def __init__(self, bucket_name='media'):
         if not SAEBucket.conn:
             SAEBucket.conn = storage.Connection()
         if not bucket_name:
             bucket_name = 'media'
-        self.__bucket = conn.get_bucket(bucket_name)
+        self.__bucket = SAEBucket.conn.get_bucket(bucket_name)
 
     def put_object(self, name, content): 
         self.__bucket.put_object(name, content)
