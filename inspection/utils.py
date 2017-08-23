74ab1456856fa4714c00bdd32b8d36bdb2c700c5
Nobody <nobody@yq150.onlinetools>
2017-08-24 01:07:21 +0800

    git commit by online edit file 1/inspection/utils.py

--- inspection/utils.py
+++ inspection/utils.py
@@ -69,7 +69,7 @@ def file_cleanup2(sender, **kwargs):
                 
                 if not m.filter(**{'%s__exact' % fieldname: getattr(inst_raw, fieldname)})\
                 .exclude(pk=inst_raw._get_pk_val()):
-                    #raise RuntimeError('env setup %s' % getattr(inst_raw, fieldname))
+                    SAEBucket().delete(path)
                     try:
                         if 'SERVER_SOFTWARE' in os.environ:                            
                             raise RuntimeError('env setup %s' % path)
