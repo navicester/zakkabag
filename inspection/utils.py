8ff4e33705f2a6e563b5c311f0921674fd84e4fc
Nobody <nobody@yq150.onlinetools>
2017-08-24 01:04:37 +0800

    git commit by online edit file 1/inspection/utils.py

--- inspection/utils.py
+++ inspection/utils.py
@@ -63,7 +63,11 @@ def file_cleanup2(sender, **kwargs):
                 else:
                     if not (hasattr(f, 'path') and os.path.exists(f.path)):
                         continue
-                raise RuntimeError('env setup %s' % getattr(inst_raw, fieldname))
+                
+                if getattr(inst_raw, fieldname) == getattr(inst, fieldname):
+                    raise RuntimeError('env setup %s' % getattr(inst_raw, fieldname))
+                    continue
+                
                 if getattr(inst_raw, fieldname) != getattr(inst, fieldname) \
                 and not m.filter(**{'%s__exact' % fieldname: getattr(inst_raw, fieldname)})\
                 .exclude(pk=inst_raw._get_pk_val()):
