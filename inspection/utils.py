157821eb45fcc12eb66858db12128c5e4edda6d5
Nobody <nobody@yq150.onlinetools>
2017-08-24 01:00:10 +0800

    git commit by online edit file 1/inspection/utils.py

--- inspection/utils.py
+++ inspection/utils.py
@@ -58,6 +58,7 @@ def file_cleanup2(sender, **kwargs):
                 path = None
                 if 'SERVER_SOFTWARE' in os.environ:                     
                     path = SAEBucket().url(f.name)
+                    raise RuntimeError('env setup' % path)
                     if not path:
                         continue
                 else:
